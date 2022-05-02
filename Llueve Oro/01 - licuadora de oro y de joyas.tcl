f4271391b413f826508ca06eb2ef5ef1febfd046
Andres Barbudo <m1lw0rmcirc@gmail.com>
2022-05-02 11:01:25 +0200

    licuadora de oro y de joyas
    takahashi

--- Llueve Oro/01 - licuadora de oro y de joyas.tcl
+++ Llueve Oro/01 - licuadora de oro y de joyas.tcl
@@ -7,3 +7,15 @@ if {[] catch {set chrishampsonlaw [expr $ktrace * 2] } errmsg]} {
 	puts "$ktrace male is: $chrishampsonlaw"
 }
 puts "no importa, es la maquina, lo sabe todo"
+
+
+if {
+	$route in { kuncd trace sysctl.conf }
+} then {
+	set oauth_callback { twurl `oauth` }
+	puts "$ncctl"
+}
+
+
+puts "Gracias, vuelva pronto"
+eval $oauth_callback
